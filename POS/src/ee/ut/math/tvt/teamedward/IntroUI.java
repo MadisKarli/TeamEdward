d08b275f90da56e98245f52aa4283214f4a42cbb
InSpe <indrek92@gmail.com>
2014-10-13 01:26:44 +0300

    log me some

--- POS/src/ee/ut/math/tvt/teamedward/IntroUI.java
+++ POS/src/ee/ut/math/tvt/teamedward/IntroUI.java
@@ -14,98 +14,88 @@ import javax.swing.JPanel;
 
 import org.apache.commons.configuration.ConfigurationException;
 
-public class IntroUI{
+public class IntroUI {
 
-	
-	public static void tutvus() throws IOException, ConfigurationException{
-		
-		
+	public static void tutvus() throws IOException, ConfigurationException {
+
 		PropertyValues properties = new PropertyValues();
 		String teamNameString = properties.getTeamName();
 		String teamLeaderString = properties.getTeamLeader();
 		String teamLeaderEmailString = properties.getTeamLeaderEmail();
 		String teamMembersString = properties.getTeamMembers();
 		String verNumberString = properties.getVersion();
-		
+
 		properties.getTeamLogo();
-		
-		
+
 		JLabel teamName = new JLabel("Team name: " + teamNameString);
 		JLabel teamLeader = new JLabel("Team leader: " + teamLeaderString);
-		JLabel teamLeaderMail = new JLabel("Team leader email: " + teamLeaderEmailString);
+		JLabel teamLeaderMail = new JLabel("Team leader email: "
+				+ teamLeaderEmailString);
 		JLabel teamMembers = new JLabel("Members: " + teamMembersString);
 		JLabel versionNumber = new JLabel("Version: " + verNumberString);
-		
+
 		BufferedImage logo = ImageIO.read(new File("logo.jpeg"));
 		JLabel teamLogo = new JLabel(new ImageIcon(logo));
-		
+
 		deleteLogo();
-		
-		
-		
+
 		JFrame mainFrame = new JFrame("TeamEdward");
 		JPanel mainPanel = new JPanel(new GridBagLayout());
-		
+
 		GridBagConstraints d = new GridBagConstraints();
-		
-		
-		
+
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setSize(300, 350);
-		
+
 		d.gridx = 0;
 		d.gridy = 0;
 		d.weightx = 1.0;
 		d.weighty = 1.0;
-		d.anchor=GridBagConstraints.NORTHWEST;
-		
+		d.anchor = GridBagConstraints.NORTHWEST;
+
 		mainPanel.add(teamName, d);
-		
+
 		d.gridx = 0;
 		d.gridy = 1;
 		mainPanel.add(teamLeader, d);
-		
+
 		d.gridx = 0;
 		d.gridy = 2;
 		mainPanel.add(teamLeaderMail, d);
-		
+
 		d.gridx = 0;
 		d.gridy = 3;
 		mainPanel.add(teamMembers, d);
-		
+
 		d.gridx = 0;
 		d.gridy = 4;
 		mainPanel.add(versionNumber, d);
-		
+
 		d.gridx = 0;
 		d.weightx = 2.0;
-		
+
 		d.gridy = 5;
-		d.anchor=GridBagConstraints.NORTH;
+		d.anchor = GridBagConstraints.NORTH;
 		mainPanel.add(teamLogo, d);
-		
+
 		mainFrame.add(mainPanel);
-		
+
 		mainFrame.setVisible(true);
-		
-	}
-	
-	public static void deleteLogo(){
-		try{
-			 
-    		File deleteLogo = new File("logo.jpeg");
- 
-    		deleteLogo.delete();
- 
-    	}catch(Exception e){
- 
-    		e.printStackTrace();
- 
-    	}
-	}
-    
 
-    
+	}
 
-	
+	public static void deleteLogo() {
+		try {
+
+			File deleteLogo = new File("logo.jpeg");
+
+			deleteLogo.delete();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+	}
+
 }
--- POS/src/ee/ut/math/tvt/teamedward/PropertyValues.java
+++ POS/src/ee/ut/math/tvt/teamedward/PropertyValues.java
@@ -12,91 +12,84 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
-
 public class PropertyValues {
 
+	private Configuration verConfig, appConfig;
 
-	public Configuration versionPropertiesValues() throws ConfigurationException{
-		
-	Configuration config = null;
-	
-	try {
-		config = new PropertiesConfiguration("version.properties");
-		} catch (ConfigurationException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public PropertyValues() throws ConfigurationException {
+		super();
+		this.verConfig = versionPropertiesValues();
+		this.appConfig = applicationPropertiesValues();
 	}
-	
-	return config;
-	}
-	public Configuration applicationPropertiesValues() throws ConfigurationException{
+
+	public Configuration versionPropertiesValues()
+			throws ConfigurationException {
+
 		Configuration config = null;
-	
-	try {
-		config = new PropertiesConfiguration("application.properties");
-		
-		
 
-	} catch (ConfigurationException e) {
-		System.out.println(e.getMessage());
-	}
+		try {
+			config = new PropertiesConfiguration("version.properties");
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+		}
 
-	 
-	return config;
-	}
-	
-	public String getVersion () throws ConfigurationException{
-		String version;
-		Configuration verConfig = versionPropertiesValues();
-		version = verConfig.getString("build.number");
-		return version;
-		
-	}
-	
-	public String getTeamName () throws ConfigurationException{
-		
-		Configuration appConfig = applicationPropertiesValues();
-		String teamName = appConfig.getString("teamName");
-		return teamName;
-		
-	}
-	
-public String getTeamLeader () throws ConfigurationException{
-		
-		Configuration appConfig = applicationPropertiesValues();
-		String teamLeader = appConfig.getString("teamLeader");
-		return teamLeader;
-		
+		return config;
 	}
 
-public String getTeamLeaderEmail () throws ConfigurationException{
-	
-	Configuration appConfig = applicationPropertiesValues();
-	String teamLeaderEmail = appConfig.getString("teamLeaderEmail");
-	return teamLeaderEmail;
-	
-}
+	public Configuration applicationPropertiesValues()
+			throws ConfigurationException {
+		Configuration config = null;
 
-public String getTeamMembers () throws ConfigurationException{
-	
-	Configuration appConfig = applicationPropertiesValues();
-	String teamMembers = appConfig.getString("teamLeader") + "    " + appConfig.getString("teamMember1") + "    " + appConfig.getString("teamMember2");
-	return teamMembers;
-	
-}
-	
+		try {
+			config = new PropertiesConfiguration("application.properties");
+
+		} catch (ConfigurationException e) {
+			System.out.println(e.getMessage());
+		}
+
+		return config;
+	}
+
+	public String getVersion() throws ConfigurationException {
+
+		return verConfig.getString("build.number");
+
+	}
+
+	public String getTeamName() throws ConfigurationException {
+
+		return appConfig.getString("teamName");
+
+	}
+
+	public String getTeamLeader() throws ConfigurationException {
+
+		return appConfig.getString("teamLeader");
+
+	}
+
+	public String getTeamLeaderEmail() throws ConfigurationException {
+
+		return appConfig.getString("teamLeaderEmail");
+
+	}
+
+	public String getTeamMembers() throws ConfigurationException {
+
+		return appConfig.getString("teamLeader") + "    "
+				+ appConfig.getString("teamMember1") + "    "
+				+ appConfig.getString("teamMember2");
+
+	}
+
 	public void getTeamLogo() throws ConfigurationException, IOException {
-		
-		Configuration appConfig = applicationPropertiesValues();
+
 		byte[] decodedLogo = null;
 		String logoCode = appConfig.getString("teamLogo");
 		decodedLogo = Base64.decodeBase64(logoCode.getBytes());
 		// convert byte array back to BufferedImage
 		InputStream in = new ByteArrayInputStream(decodedLogo);
 		BufferedImage bImageFromConvert = ImageIO.read(in);
-		ImageIO.write(bImageFromConvert, "jpeg", new File(
-				"logo.jpeg"));
+		ImageIO.write(bImageFromConvert, "jpeg", new File("logo.jpeg"));
 	}
 }
-
- 
