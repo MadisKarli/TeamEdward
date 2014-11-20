3a00bca4d02a9dcd9bd31269123d9ace6d57aa7d
MadisKarli <matukoppel@gmail.com>
2014-11-20 12:22:42 +0200

    Do you push me now? DO YOU PUSH ME NOW?

--- POS/src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
+++ POS/src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -1,12 +1,6 @@
 package ee.ut.math.tvt.salessystem.domain.controller.impl;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 
 import org.hibernate.Session;
 
