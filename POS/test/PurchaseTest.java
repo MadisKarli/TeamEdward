33c8bfa3d30481afa52a89e858366149d6dd7a68
MadisKarli <matukoppel@gmail.com>
2014-11-20 12:23:27 +0200

    DO YOU PUSH ME NOW?!

---
+++ POS/test/PurchaseTest.java
@@ -0,0 +1,25 @@
+package ee.ut.math.tvt.salessystem.domain.data;
+
+public class PurchaseTest {
+	@Before
+	public void setUp() {
+
+	}
+
+	// TODO
+	@Test
+	public void testAddSoldItem()() {
+	}
+
+	// TODO
+	public void testGetSumWithNoItems() {
+	}
+
+	// TODO
+	public void testGetSumWithOneItem() {
+	}
+	
+	// TODO
+	public void testGetSumWithMultipleItems() {
+	}
+}
