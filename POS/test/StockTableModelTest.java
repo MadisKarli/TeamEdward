63ae56bd4934ea1688ac5d398260410f34a5ac36
MadisKarli <matukoppel@gmail.com>
2014-11-24 18:03:48 +0200

    Better StockTableModelTest

--- POS/test/StockTableModelTest.java
+++ POS/test/StockTableModelTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.NoSuchElementException;
 
@@ -14,46 +16,45 @@ public class StockTableModelTest {
 	private StockItem stock1;
 	private StockItem stock2;
 	private StockItem stock3;
-	long i = 12345678910L;
-	long j = 12314L;
+	long i = (long)1;
+	long j = (long)2;
+	long k = (long)3;
 
 	@Before
 	public void setUp() throws VerificationFailedException {
 		test1 = new StockTableModel();
-		stock1 = new StockItem(i, "stock1", "", 12.0, 5); //unique
-		stock2 = new StockItem(j, "stock1", "", 3.0, 3); //same name as stock1
+		stock1 = new StockItem(i, "stock1", "", 12.0, 5); // unique
+		stock2 = new StockItem(j, "stock1", "", 3.0, 3); // same name as stock1
 		test1.addItem(stock1);
-
 
 	}
 
-	// TODO
 	@Test(expected = VerificationFailedException.class)
 	public void testValidateNameUniqueness() throws VerificationFailedException {
-		//trying to add item with name already in use
+		// trying to add item with name already in use
 		test1.addItem(stock2);
 	}
 
 	// TODO
 	@Test
 	public void testHasEnoughInStock() throws VerificationFailedException {
-		//not sure what is expected here, just adding item and cheking stock 	
-		stock3 = new StockItem(j, "stock3", "", 14, 10);
+		//since we don't have max or min values we cannot implement it correctly
+		//testing with 0 qty and 5 qty and checking wheter they are bigger than 0 or not
+		stock3 = new StockItem(k, "stock3", "", 15.0, 0);
 		test1.addItem(stock3);
-		assertEquals(test1.getItemById(j).getQuantity(), 10, 0.001);
+		assertTrue(test1.getItemById(i).getQuantity() >0);
+		assertFalse(test1.getItemById(k).getQuantity()>0);
 	}
 
-	// TODO
 	@Test
 	public void testGetItemByIdWhenItemExists() {
 		stock3 = test1.getItemById(i);
 		assertEquals(stock3.getName(), stock1.getName());
 	}
 
-	// TODO
 	@Test(expected = NoSuchElementException.class)
 	public void testGetItemByIdWhenThrowsException() {
-		stock3 = test1.getItemById(1);
-		
+		stock3 = test1.getItemById(3);
+
 	}
 }
